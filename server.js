318e28e4c900365f7018214d9193bacdd41ab7ef
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2015-06-18 12:51:31 -0400

    all set for dotcloud

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 var express 	= require('express'),
 	bodyParser 	= require('body-parser'),
-	mongoose    = require('mongoose');
+	mongoose    = require('mongoose'),
+	fs			= require('fs');
+
+var envFile = './environment.json';
+
+var env;
+
+try {
+	env = JSON.parse(fs.readFileSync(envFile));
+}
+catch (err) {
+	console.log(err.stack);
+	env = {};
+}
+
+console.log('env:');
+console.log(env);
 
 // db
-// mongoose.connect('');
+var mon_options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
+mongoose.connect(env.MONGO_URI, mon_options);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback () {
+
+});
+
+// app
 
 var app	= express();
 

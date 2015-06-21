bd76c45a0e31ec8ee0079c779280c68938268e9d
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2015-06-20 23:34:12 -0400

    jsonwebtoken dep

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express     = require('express'),
     mongoose    = require('mongoose'),
     fs          = require('fs');
 
+// read environment variables
+
 var envFile = './environment.json';
 
 var env;
@@ -15,22 +17,9 @@ catch (err) {
     env = {};
 }
 
-console.log('env:');
-console.log(env);
-
 // db
-var mon_options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 
-mongoose.connect(env.MONGO_URI, mon_options);
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function callback () {
-
-});
+mongoose.connect(env.MONGO_URI);
 
 // app
 
@@ -41,13 +30,43 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
+// models
+
+var Item = require('./app/models/item');
+
 // routes
+
 var router = express.Router();
 
-// test
-router.get('/', function(req, res) {
-    res.json({ message: 'Nourish API' });
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
 });
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Nourish API' });   
+});
+
+router.route('/items')
+
+    // create a menu item
+    .post(function(req, res) {
+        
+        var item = new Item();      // create a new instance of the Item model
+        item.name = req.body.name;  // set the item's name (comes from the request)
+
+        // save the item and check for errors
+        item.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Item created!' });
+        });
+        
+    });
+
 
 // register routes
 app.use('/api', router);
